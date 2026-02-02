f5775fb3e927e18e93db801b44c0adefa1b1d3b9
Aрина Шейнова <166861256+lenardli@users.noreply.github.com>
2026-02-02 23:54:00 +0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo systemctl status docker
+sudo systemctl start docker
+
 sudo docker build -t my-whisper-app .
 sudo docker run -d -p 7860:7860 --name whisper-container my-whisper-app
