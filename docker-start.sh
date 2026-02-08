4fe5e843d54c8b6fc909c67c87f7504dd125ad9c
Aрина Шейнова <166861256+lenardli@users.noreply.github.com>
2026-02-08 22:54:01 +0300

    Update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build -t my-whisper-app .
-sudo docker run -d -p 7860:7860 --name whisper-container my-whisper-app
+sudo docker run -dit -p 7860:7860 --name whisper-container -v ./outputs/:/Whisper-WebUI/outputs -v ./models/:/Whisper-WebUI/models my-whisper-app
